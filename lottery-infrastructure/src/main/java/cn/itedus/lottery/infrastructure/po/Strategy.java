39ccbe6240e5913f78920fc81329f45b25785f94
Levi <levitan.dev@gmail.com>
2023-06-27 23:23:26 +0800

    修复抽奖发奖bug

--- lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/po/Strategy.java
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/po/Strategy.java
@@ -2,34 +2,54 @@ package cn.itedus.lottery.infrastructure.po;
 
 import java.util.Date;
 
-
+/**
+ * @description: 策略配置
+ */
 public class Strategy {
 
-    // 自增ID
+    /**
+     * 自增ID
+     */
     private Long id;
 
-    // 策略ID
+    /**
+     * 策略ID
+     */
     private Long strategyId;
 
-    // 策略描述
+    /**
+     * 策略描述
+     */
     private String strategyDesc;
 
-    // 策略方式「1:单项概率、2:总体概率」
+    /**
+     * 策略方式「1:单项概率、2:总体概率」
+     */
     private Integer strategyMode;
 
-    // 发放奖品方式「1:即时、2:定时[含活动结束]、3:人工」
+    /**
+     * 发放奖品方式「1:即时、2:定时[含活动结束]、3:人工」
+     */
     private Integer grantType;
 
-    // 发放奖品时间
+    /**
+     * 发放奖品时间
+     */
     private Date grantDate;
 
-    // 扩展信息
+    /**
+     * 扩展信息
+     */
     private String extInfo;
 
-    // 创建时间
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
-    // 修改时间
+    /**
+     * 修改时间
+     */
     private Date updateTime;
 
     public Long getId() {
@@ -102,20 +122,5 @@ public class Strategy {
 
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Strategy{" +
-                "id=" + id +
-                ", strategyId=" + strategyId +
-                ", strategyDesc='" + strategyDesc + '\'' +
-                ", strategyMode=" + strategyMode +
-                ", grantType=" + grantType +
-                ", grantDate=" + grantDate +
-                ", extInfo='" + extInfo + '\'' +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
     }
 }
