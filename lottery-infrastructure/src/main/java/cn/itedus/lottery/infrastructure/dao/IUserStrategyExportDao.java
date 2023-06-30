cf7b3e7b23e7ddca94896c9f7f21d7f0b977d61a
levitan <levitan755@163.com>
2023-06-30 16:32:37 +0800

    分表

---
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IUserStrategyExportDao.java
@@ -0,0 +1,33 @@
+package cn.itedus.lottery.infrastructure.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
+import cn.itedus.lottery.infrastructure.po.UserStrategyExport;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户策略计算结果表
+ * 分表
+ * DBRouterStrategy注解为分表注解
+ */
+@Mapper
+@DBRouterStrategy(splitTable = true)
+public interface IUserStrategyExportDao {
+
+    /**
+     * 插入一条用户策略计算结果数据
+     * 插入操作需要用到分库分表
+     * @param userStrategyExport
+     */
+    @DBRouter(key = "uId")
+    void insert(UserStrategyExport userStrategyExport);
+
+    /**
+     * 查询数据
+     * @param uId 用户id
+     * @return 用户策略
+     */
+    @DBRouter
+    UserStrategyExport queryUserStrategyExportByUId(String uId);
+
+}
